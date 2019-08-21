9484116da9fa6de51116a77c724d272eec96493a
Vasily Sotnikov <vasily.sotnikov@physik.uni-freiburg.de>
2019-08-21 23:51:06 +0200

    remove more latexsuite

--- .vim/vimconfig/vimconfig.sh
+++ .vim/vimconfig/vimconfig.sh
@@ -4,14 +4,5 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-#setup vim-latex
-cd $HOME/.vim
-tar xf vim-latex-1.9.0.tar.gz
-cp -R vim-latex-1.9.0/. ./
-rm -rf vim-latex-1.9.0
-rm vim-latex-1.9.0.tar.gz
-
 cd -
 ./installYCM.sh
-./setupYCMGenerator.sh
-./build_bear.sh
