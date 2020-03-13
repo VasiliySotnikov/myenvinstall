eee02d744ad24aecf0e146f65edd549fa60d529b
Vasily Sotnikov <sotnikov@mpp.mpg.de>
2020-03-13 19:13:32 +0100

    Update build_vim

--- build_vim.sh
+++ build_vim.sh
@@ -9,20 +9,12 @@ git pull
 
 make distclean
 
-PYCFG=$(dpkg -S "*python3*config.c" | sed "s/.*:\ \(\/.*\)config\.c/\1/")
+#PYCFG=$(dpkg -S "*python3*config.c" | sed "s/.*:\ \(\/.*\)config\.c/\1/")
 
 ./configure --with-features=huge --enable-multibyte --enable-rubyinterp=yes \
-    --with-python3-config-dir=$PYCFG \
     --enable-python3interp=yes --enable-perlinterp=yes --enable-luainterp=yes \
     --enable-gui=gtk2 --enable-cscope --prefix=$HOME/local
 
-make VIMRUNTIMEDIR=$HOME/local/share/vim/vim80 -j5
+#make VIMRUNTIMEDIR=$HOME/local/share/vim/vim82 -j5
+make -j6
 make install
-
-#cd $HOME/.vim/bundle/
-#git clone https://github.com/Valloric/YouCompleteMe.git || true
-#cd YouCompleteME
-#git pull
-#git submodule update --init --recursive
-#make distclean
-#python3 install.py --clang--completer
