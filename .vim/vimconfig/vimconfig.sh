b64dd9468692e22f15c3dbf9cd5bee936e5f2f8b
Vasily Sotnikov <sotnikov@mpp.mpg.de>
2020-06-07 15:01:21 +0200

    fix issue with installation of coc dependencies

--- .vim/vimconfig/vimconfig.sh
+++ .vim/vimconfig/vimconfig.sh
@@ -5,7 +5,8 @@ set -e
 
 # setup all plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 2>/dev/null || (cd ~/.vim/bundle/Vundle.vim ; git pull)
-vim +PluginInstall +PluginUpdate +qall
+vim +PluginInstall
 
-vim +"CocInstall coc-cmake coc-python coc-clangd coc-json coc-rls" +qall
-vim -c "CocCommand clangd.install" -c "qa" t.cpp
+vim +":call coc#util#install()"
+vim +"CocInstall coc-cmake coc-python coc-clangd coc-json coc-rls"
+vim -c "CocCommand clangd.install" t.cpp
