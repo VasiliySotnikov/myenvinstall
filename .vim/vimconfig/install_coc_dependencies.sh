0f8609efa2e64ec4e56ec91258cb45ec9a3710e5
Vasily Sotnikov <sotnikov@mpp.mpg.de>
2020-06-26 15:31:15 +0200

    Trying newer version of lsw-wl, not working

--- .vim/vimconfig/install_coc_dependencies.sh
+++ .vim/vimconfig/install_coc_dependencies.sh
@@ -8,6 +8,6 @@ pip3 install --user fortran-language-server
 
 echo "Mathematica stuff:"
 cd "$HOME/scratch"; 
-git clone https://github.com/kenkangxgwe/lsp-wl.git 2>/dev/null || (cd lsp-wl ; git pull)
+git clone -b develop https://github.com/kenkangxgwe/lsp-wl.git 2>/dev/null || (cd lsp-wl ; git pull)
 cd - 
 math -script pacelets.m
