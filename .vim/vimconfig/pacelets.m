6028df3680c02bc3346f9439b1573d75c4135f7a
Vasily Sotnikov <sotnikov@mpp.mpg.de>
2021-03-31 13:49:00 +0200

    Update pacelets installation

--- .vim/vimconfig/pacelets.m
+++ .vim/vimconfig/pacelets.m
@@ -1,7 +1,7 @@
-options = {UpdatePacletSites -> True};
+Block[{options = {UpdateSites -> True}},
 
-PacletInstall["https://github.com/WolframResearch/GitLink/releases/download/v0.1.1/GitLink-2018.07.20.01.paclet", options]
-PacletInstall["CodeParser", options]
-PacletInstall["CodeInspector", options]
+PacletInstall["https://github.com/WolframResearch/GitLink/releases/download/v0.1.1/GitLink-2018.07.20.01.paclet", options];
+PacletInstall["CodeParser", options];
+PacletInstall["CodeInspector", options];
 
-Remove[options];
+];
